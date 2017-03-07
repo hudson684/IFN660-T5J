d97dbc0b6a1705b73304eb76b37b707153a9bf65
An Tran <tranan@Trans-MacBook-Air.local>
2017-03-07 21:06:17 +1000

    Updated
    Change the project to match first week tutorial

---
+++ Project Folder/IFN660_Java_ECMAScript/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFN660_Java_ECMAScript
+{
+    public enum Tokens
+    {
+        EOF = 264, WHILE = 265, IDENT = 266, GE = 267, NUMBER = 268
+    };
+
+    public struct MyValueType
+    {
+        public int num;
+        public string name;
+    };
+
+    public abstract class ScanBase
+    {
+        public MyValueType yylval;
+        public abstract int yylex();
+        protected virtual bool yywrap() { return true; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scanner scanner = new Scanner(
+    new System.IO.FileStream(args[0], System.IO.FileMode.Open));
+
+            Tokens token;
+            do
+            {
+                token = (Tokens)scanner.yylex();
+                switch (token)
+                {
+                    case Tokens.NUMBER:
+                        Console.WriteLine("NUMBER ({0})", scanner.yylval.num);
+                        break;
+                    case Tokens.IDENT:
+                        Console.WriteLine("IDENT ({0})", scanner.yylval.name);
+                        break;
+                    case Tokens.WHILE:
+                        Console.WriteLine("WHILE");
+                        break;
+                    case Tokens.GE:
+                        Console.WriteLine("GE");
+                        break;
+                    case Tokens.EOF:
+                        Console.WriteLine("EOF");
+                        break;
+                    default:
+                        Console.WriteLine("'{0}'", token);
+                        break;
+                }
+            }
+            while (token != Tokens.EOF);
+
+        }
+    }
+
+}
---
+++ Project Folder/IFN660_Java_ECMAScript/scanner.lex
@@ -0,0 +1,29 @@
+﻿%namespace IFN660_Java_ECMAScript
+
+digit [0-9]
+letter [a-zA-Z]
+
+%%
+\/\*.\*\/				/* Comments */
+while                        { return (int)Tokens.WHILE; }
+
+{letter}({letter}|{digit})* { yylval.name = yytext; return (int)Tokens.IDENT; }
+
+{digit}+	    { yylval.num = int.Parse(yytext); return (int)Tokens.NUMBER; }
+
+
+,                            { return ','; }
+
+>=                           { return (int)Tokens.GE; }
+
+>                            { return '>'; }
+
+[ \r\n\t]                    /* skip whitespace */
+
+.                            { 
+                                 throw new Exception(
+                                     String.Format(
+                                         "unexpected character '{0}'", yytext)); 
+                             }
+
+%%
