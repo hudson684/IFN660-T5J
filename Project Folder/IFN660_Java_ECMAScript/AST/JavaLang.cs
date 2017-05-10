c99df75a7d6e33a161e0cbe34e634d5f7a43fa9c
manworldloves <themantheworldloves@gmail.com>
2017-05-10 23:27:51 +1000

    Added java_lang.cs file

---
+++ Project Folder/IFN660_Java_ECMAScript/AST/JavaLang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFN660_Java_ECMAScript.AST
+{
+    // this class manages all things related to the Java.Lang standard library
+    class JavaLang: Declaration
+    {
+        public static LexicalScope getJavaLangScope()
+        {
+            var rootScope = new LexicalScope();
+
+            return rootScope;
+        }
+
+        public void AddItemsToSymbolTable(LexicalScope scope)
+        {
+            scope.Symbol_table.Add("String", this);
+            scope.Symbol_table.Add("System.out.println", this);
+        }
+
+        public Type ObtainType()
+        {
+            return new NamedType("class");
+        }
+    }
+}
