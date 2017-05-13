9bd15edd4d71c272e9203cf42e76d70c70242e1c
ktrannt <ktrannt@gmail.com>
2017-05-13 14:04:25 +1000

    Initial work on SwitchLabelStatement

--- Project Folder/IFN660_Java_ECMAScript/AST/SwitchStatement.cs
+++ Project Folder/IFN660_Java_ECMAScript/AST/SwitchStatement.cs
@@ -29,32 +29,42 @@ namespace IFN660_Java_ECMAScript.AST
 
             return expression.ResolveNames(scope) & loopResolve;
         }
+        public override void TypeCheck()
+        {
+
+        }
     }
 
     public class SwitchLabelStatement : Statement
     {
-
+        private Expression switchValue;
+        private Boolean switchLabel; 
         //this exists for case with Expression
         public SwitchLabelStatement(Expression switchValue)
         {
-
+            this.switchValue = switchValue;
+            switchLabel = true; 
         }
 
         //this exists for case with Constant Name
         public SwitchLabelStatement(String switchValueName)
         {
-
+            // Ignore this rabbit hole for now
         }
 
         //this exists for the default case
         public SwitchLabelStatement()
         {
-
+            switchLabel = false; 
         }
 
         public override bool ResolveNames(LexicalScope scope)
         {
             return true;
+        }
+        public override void TypeCheck()
+        {
+
         }
     }
 
